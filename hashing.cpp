3f9b3a75eb96fd2b80876ef0c07b569b74e4d7e4
HN026 <plancksconstant01@gmail.com>
2022-10-11 12:28:20 +0530

    commit

---
+++ hashing.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+// It will try to search the trees less than logn time.
+// Drawback of hashing is that a lot of space gets wasted in it.
+// In hashing when two keys are mapped at same position, we call it collision. (It happens in many-one type of mapping.)
+
+
+int main()
+{
+    return 0;
+}
